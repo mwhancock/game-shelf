b3e950363ec84d69cf9cb3aa71222b8350478e52
Mark <mwhancock2@gmail.com>
2023-03-28 21:54:39 -0230

    modify api query to only return game array

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA")
-    .then(res => res.json())
-    .then(data => console.log(data))
+fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA")
+    .then((response) => response.json())
+    .then((data) => console.log(data.games))
