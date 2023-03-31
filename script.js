475a113937e831bc72c29bfc1338e19b531151d2
Mark <mwhancock2@gmail.com>
2023-03-30 23:41:52 -0230

    implement gallery, start working on manual control

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
+
+
 fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA")
     .then((response) => response.json())
     .then((data) => console.log(data.games))
 
-    let picIndex = 0;
-    slideShow();
+let slideIndex = 0;
+slideShow();
 
-    function slideShow(){
-        let i;
-        let slides = document.getElementsByClassName("feature-card");
-        for(i = 0;  i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        picIndex++;
-        if(picIndex > slides.length){picIndex = 1}
-        slides[picIndex-1].style.display = "block";
-        setTimeout(slideShow, 5000);
-
+function slideShow(){
+    let i;
+    let slides = document.getElementsByClassName("feature-card");
+    for(i = 0;  i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if(slideIndex > slides.length){slideIndex = 1};
+    slides[slideIndex-1].style.display = "block";
+    let slider = setInterval(slideShow, 4000);
+    slides.addEventListener("mouseover", (e) => {clearInterval(slider)});
 
-    function galleryNav(){
-        
-    }
+}
 
+function currentSlide(n){
+
+}
+
+function nextSlide(n){
+
+}
