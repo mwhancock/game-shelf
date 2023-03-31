fa2d798918c226474ea818c155b0be12d0bbf7eb
Mark <mwhancock2@gmail.com>
2023-03-31 16:46:48 -0230

    refactor - section off code by comments

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-
+// Import api and console log game data to console
 fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA")
     .then((response) => response.json())
     .then((data) => console.log(data.games))
 
+ // Targets the "Feature Card" and initializes the carousel index at 0
 let slides = document.getElementsByClassName("feature-card");
 let slideIndex = 0;
 slideShow();
 
+// Slideshow automation -- sets display none, then loops through displaying next card every 4 seconds
 function slideShow(){
     let i;
     for(i = 0;  i < slides.length; i++) {
@@ -17,10 +19,10 @@ function slideShow(){
     if(slideIndex > slides.length){slideIndex = 1};
     slides[slideIndex-1].style.display = "block";
     let slider = setInterval(slideShow, 4000);
-    // slides.addEventListener("mouseover", clearInterval(slider));
-
+    // pauseSlideShow(slider);
 }
 
+// Manual controls for slide show
 function prevSlide(){
     let i;
     for(i = 0;  i < slides.length; i++) {
@@ -40,3 +42,7 @@ function nextSlide(){
     if(slideIndex > slides.length){slideIndex = 1};
     slides[slideIndex-1].style.display = "block";
 }
+
+// function pauseSlideShow(n){
+//     slides.addEventListener("mouseover", clearInterval(n))
+// }
