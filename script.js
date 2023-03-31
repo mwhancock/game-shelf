49a5fac787bbf45b906a01989d13efe6e2702073
Mark <mwhancock2@gmail.com>
2023-03-31 16:07:04 -0230

    implement manual controls to gallery slide

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9
     .then((response) => response.json())
     .then((data) => console.log(data.games))
 
+let slides = document.getElementsByClassName("feature-card");
 let slideIndex = 0;
 slideShow();
 
 function slideShow(){
     let i;
-    let slides = document.getElementsByClassName("feature-card");
     for(i = 0;  i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -17,14 +17,26 @@ function slideShow(){
     if(slideIndex > slides.length){slideIndex = 1};
     slides[slideIndex-1].style.display = "block";
     let slider = setInterval(slideShow, 4000);
-    slides.addEventListener("mouseover", (e) => {clearInterval(slider)});
+    // slides.addEventListener("mouseover", clearInterval(slider));
 
 }
 
-function currentSlide(n){
-
+function prevSlide(){
+    let i;
+    for(i = 0;  i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex--;
+    if(slideIndex < 0){slideIndex = -10;}
+    slides[slideIndex-1].style.display = "block"
 }
 
-function nextSlide(n){
-
+function nextSlide(){
+    let i;
+    for(i = 0;  i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length){slideIndex = 1};
+    slides[slideIndex-1].style.display = "block";
 }
