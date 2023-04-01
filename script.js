678ea2fe61b3e8ddd13474b8e3b7a165672623f3
Mark <mwhancock2@gmail.com>
2023-04-01 12:29:32 -0230

    refactor

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// Import api and console log game data to console
+// fetch game collection, logs the name of each game in list
 async function getGameList(){
     const response = await fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA");
     const data = await response.json();
-    let gameList = data.games
-    for(let gameIndex = 0; gameIndex < gameList.length; gameIndex++){
+    const gameList = data.games;
+    for(let gameIndex = 0; gameIndex <= gameList.length; gameIndex++){
         console.log(gameList[gameIndex]["name"]);
-        console.log(gameList[gameIndex]["images"]["medium"])
+    }
+}
+
+// fetch game collection, log image url for each game in list
+async function getGameArt(){
+    const response = await fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA");
+    const data = await response.json();
+    const artList = data.games;
+    for(let gameIndex = 0; gameIndex <= artList.length; gameIndex++){
+        console.log(artList[gameIndex]["images"]["medium"]);
     }
 }
  
 getGameList();
+getGameArt();
 
  // Targets the "Feature Card" and initializes the carousel index at 0
 let slides = document.getElementsByClassName("feature-card");
