0acdc0a0af754751e9389acb6cbdbcc015bed029
Mark <mwhancock2@gmail.com>
2023-04-05 17:23:33 -0230

    start writing function to add feature card templates to gallery

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-// Global Variables
-
-
 // Grab HTML element that will display users libre=ary preview when available
-const libraryPreview = document.getElementById("library-preview")
+const libraryPreview = document.getElementById("library-preview");
+
+const gallery = document.getElementById("gallery");
 
 // Grab our HTML Element that is going to display APi data once available
-const count_display = document.getElementById('count-display');
+const count_display = document.getElementById("count-display");
 
+// // Add an event listener to our HTML element that needs the API data
+// count_display.addEventListener('games_retrieved', (event) => {
+//     console.log(event)
+//     // Set the element's value to our API data
+//     count_display.innerHTML = `Game Data Retrieved for ${event.detail?.games?.length} Games`
+// })
 
-// Add an event listener to our HTML element that needs the API data
-count_display.addEventListener('games_retrieved', (event) => {
-    console.log(event)
-    // Set the element's value to our API data
-    count_display.innerHTML = `Game Data Retrieved for ${event.detail?.games?.length} Games`
-})
+// fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA")
+//     .then( res => res.json() )
+//     .then( data =>
+//         {
+//             // Define the event emitter for our new custom event
+//             const gameDataRetrieved = new CustomEvent(
+//                 'games_retrieved',
+//                 {
+//                     // set our API's data into custom properties of the event's detail object
+//                     detail:
+//                     {
+// we can pass anything we want as a key-value pair here, neat!
+//             count: data.count,
+//             games: data.games
+//         }
+//     }
+// )
 
-
-fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA")
-    .then( res => res.json() )
-    .then( data =>
-        {
-            // Define the event emitter for our new custom event
-            const gameDataRetrieved = new CustomEvent(
-                'games_retrieved', 
-                {
-                    // set our API's data into custom properties of the event's detail object
-                    detail: 
-                    {
-                        // we can pass anything we want as a key-value pair here, neat!
-                        count: data.count, 
-                        games: data.games
-                    } 
-                }
-            )
-
-            // dispatch our event using the HTML object it is attached to
-            // we could also use the window object for this as well
-            count_display.dispatchEvent(gameDataRetrieved);
-        }
-    )
-    .catch( err =>
-        {
-            console.log('ERROR: ', err);
-        }
-    );
-
+// dispatch our event using the HTML object it is attached to
+// we could also use the window object for this as well
+//         count_display.dispatchEvent(gameDataRetrieved);
+//     }
+// )
+// .catch( err =>
+//     {
+//         console.log('ERROR: ', err);
+//     }
+// );
 
 // Fetch game collection, returns array of game objects
 
@@ -69,21 +65,21 @@ fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9R
 
 // const libPreview = gameArt();
 
-async function populateLibraryPreview() {
-    for(let i = 0; i <20; i++){
-        let newDiv = document.createElement("div");
-        let newAnchor = document.createElement("a");
-        let newImg = document.createElement("img");
-        newDiv.setAttribute("class", "game-card");
-        newAnchor.setAttribute("href", "game-page");
-        newImg.setAttribute("src", gameArt.then(data => {data[i]})); 
-        libraryPreview.appendChild(newDiv.appendChild(newAnchor.appendChild(newImg)));
-        // console.log(gameArt.then(data => {data[2]}))
-    }
-}
+// async function populateLibraryPreview() {
+//     for(let i = 0; i <20; i++){
+//         let newDiv = document.createElement("div");
+//         let newAnchor = document.createElement("a");
+//         let newImg = document.createElement("img");
+//         newDiv.setAttribute("class", "game-card");
+//         newAnchor.setAttribute("href", "game-page");
+//         newImg.setAttribute("src", gameArt.then(data => {data[i]}));
+//         libraryPreview.appendChild(newDiv.appendChild(newAnchor.appendChild(newImg)));
+//         // console.log(gameArt.then(data => {data[2]}))
+//     }
+// }
 
 // // Console log our API values after an arbitrary delay
-// setTimeout( 
+// setTimeout(
 //     () => {console.log({game_list, game_count})},
 //     5000
 // );
@@ -91,56 +87,60 @@ async function populateLibraryPreview() {
 // if (!is_loading) console.log({game_list, game_count})
 
 // fetch game collection, log image url for each game in list
-async function getGameArt(){
-    const response = await fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA");
-    const data = await response.json();
-    const artList = data.games;
-    for(let gameIndex = 0; gameIndex <= artList.length; gameIndex++){
-        // console.log(artList[gameIndex]["images"]["medium"]);
-    }
-    const anchor = document.createElement("a");
-    anchor.setAttribute("href", "library.html");
-    anchor.setAttribute("class", "lib-btn");
-    anchor.innerText = "Full Library";
-    libraryPreview.appendChild(anchor);
-}
+// async function getGameArt(){
+//     const response = await fetch ("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&client_id=9RQI1WBCZA");
+//     const data = await response.json();
+//     const artList = data.games;
+//     for(let gameIndex = 0; gameIndex <= artList.length; gameIndex++){
+//         // console.log(artList[gameIndex]["images"]["medium"]);
+//     }
+//     const anchor = document.createElement("a");
+//     anchor.setAttribute("href", "library.html");
+//     anchor.setAttribute("class", "lib-btn");
+//     anchor.innerText = "Full Library";
+//     libraryPreview.appendChild(anchor);
+// }
 
 // populateLibraryPreview();
 // console.log(gameArt.then(data => {data[1][1]}));
 
-
-// Slideshow automation
+// SLIDESHOW AUTOMATION
 
 // Declare variables needed for slideshow
-let slideCollection = document.getElementsByClassName("feature-card");
+let slideCollection = document.getElementsByClassName(".feature-card");
 let slideArr = Array.from(slideCollection);
 let slideIndex = 0;
 let timer;
 
-function startSlideShow(){
-    timer = setInterval(slideShow, 4000)
+function startSlideShow() {
+  timer = setInterval(slideShow, 4000);
 }
 
-function stopSlideShow(){
-    timer = clearInterval(timer)
+function stopSlideShow() {
+  timer = clearInterval(timer);
 }
 
 // Automatically moves through images
-function slideShow(){
-    slideArr.forEach((slide) => {
-        slide.style.display = "none";
-    })
-    slideIndex++;
-    if(slideIndex >= slideArr.length){slideIndex = 0};
-    slideArr[slideIndex].style.display = "block";
-    console.log(slideIndex);
+function slideShow() {
+  slideArr.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slideIndex++;
+  if (slideIndex >= slideArr.length) {
+    slideIndex = 0;
+  }
+  console.log(slideArr);
+
+  slideArr[slideIndex].style.display = "block";
 }
 
 // Manual controls for slide show
 
 // Add event listener to the next button that calls the next slide when clicked
 const nextSlide = document.querySelector(".next-btn");
-nextSlide.addEventListener("click", () => {fwdSlide()});
+nextSlide.addEventListener("click", () => {
+  fwdSlide();
+});
 
 // Add event listener to the previous button that calls the previous slide when clicked
 const prevSlide = document.querySelector(".prev-btn");
@@ -150,23 +150,47 @@ prevSlide.addEventListener("click", () => bwdSlide());
 let lastSlide = slideArr.length - 1;
 
 // Function to move to next slide
-function fwdSlide(){
-    slideArr.forEach((slide) => {
-        slide.style.display = "none";
-    })
-    slideIndex++;
-    if(slideIndex >= slideArr.length){slideIndex = 0};
-    slideArr[slideIndex].style.display = "block";
+function fwdSlide() {
+  slideArr.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slideIndex++;
+  if (slideIndex >= slideArr.length) {
+    slideIndex = 0;
+  }
+  slideArr[slideIndex].style.display = "block";
 }
 
 // Function to move to previous slide
-function bwdSlide(){
-    slideArr.forEach((slide) => {
-        slide.style.display = "none";
-    })
-    slideIndex--;
-    if(slideIndex < 0){slideIndex = lastSlide;}
-    slideArr[slideIndex].style.display = "block";
+function bwdSlide() {
+  slideArr.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = lastSlide;
+  }
+  slideArr[slideIndex].style.display = "block";
 }
 
-startSlideShow();
+
+// TEMPLATE MANIPULATION
+
+function showFeatureCards() {
+  let itemDiv, temp, imgItem, imgPath, i;
+  temp = document.getElementById("feature-card-template");
+
+  for (i = 0; i < 6; i++) {
+    itemDiv = temp.content.cloneNode(true);
+    imgItem = itemDiv.querySelector("img");
+    imgPath = `img/game${[i + 1]}.jpg`;
+    imgItem.setAttribute("src", imgPath);
+    itemDiv.appendChild(imgItem);
+    gallery.appendChild(itemDiv);
+    console.log(itemDiv)
+  }
+}
+
+showFeatureCards();
+// startSlideShow();
+
