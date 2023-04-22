58683422bbac6807fa00e21bca217d095f007eca
Mark <mwhancock2@gmail.com>
2023-04-22 15:05:32 -0230

    switch over to bgg api

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const library = document.getElementById("library-body");
 const libraryPreview = document.getElementById("recent-games");
 const gallery = document.getElementById("gallery");
+const addGameBtn = document.getElementsByClassName("new-game-btn")
 let slideArr;
 let lastSlide;
 let images = [];
@@ -11,15 +12,28 @@ let descriptions = [];
 let gameIds = [];
 
 // Add event listener to window that will populate library once api is fetched
-window.addEventListener('games_retrieved', (e) => {
+window.addEventListener('games-retrieved', (e) => {
     const gameList = e.detail.games;
-    gameList.forEach((game) => {
-        images.push(game.images.medium);
-        names.push(game.name);
-        descriptions.push(game.description_preview);
-        gameIds.push(game.id)
+    const gamePics = gameList.getElementsByTagName("image");
+    const gameNames = gameList.getElementsByTagName("name");
+    
+    for(let img in gamePics){
+        if(gamePics[img].childNodes == undefined){
+            continue
+        }
+        images.push(gamePics[img].childNodes[0].nodeValue);
+    }
+    for(let name in gameNames){
+        if(gameNames[name].childNodes == undefined){
+            continue
+        }
+        names.push(gameNames[name].childNodes[0].nodeValue);
+    }
 
-    })
+    for(let game in gameList){
+        // console.log(gameList[game])
+    }
+
     getFeatureCards();
     let slideCards = document.getElementsByClassName("gallery-img");
     slideArr = Array.from(slideCards);
@@ -31,51 +45,54 @@ window.addEventListener('games_retrieved', (e) => {
 
 // Boardgamegeek API
 
-// fetch("https://boardgamegeek.com/xmlapi2/collection?username=mwhancock&own=1")
-//     .then(res => {return res.text()})
-//     .then((data) => {
-//         const gameData = new DOMParser().parseFromString(data, "text/xml")
-//         const gameList = gameData.querySelector("items")["children"]
-//         console.log(gameList)
-//         for(let game in gameList){
-//             console.log(game[0])
+fetch("https://boardgamegeek.com/xmlapi2/collection?username=mwhancock&own=1")
+    .then(res => {return res.text()})
+    .then((data) => {
+        const gameDataRetrieved = new CustomEvent("games-retrieved",{
+            detail:{
+                games: new DOMParser().parseFromString(data, "text/xml")
+    }
+        })
+        window.dispatchEvent(gameDataRetrieved)
+    })
+    .catch(err => {
+        console.log(`ERROR: ${err}`)
+    })
+
+// fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&order_by=name_a_z&client_id=9RQI1WBCZA")
+//     .then( res => res.json() )
+//     .then( data =>
+//         { console.log(data.games)
+//             // Define the event emitter for our new custom event
+//             const gameDataRetrieved = new CustomEvent(
+//                 'games_retrieved',
+//                 {
+//                     // set our API's data into custom properties of the event's detail object
+//                     detail:
+//                     {
+//                         // we can pass anything we want as a key-value pair here, neat!
+//                         count: data.count,
+//                         games: data.games
+//                     }
+//                 }
+//             )
+
+//             // dispatch our event using the HTML object it is attached to
+            
 //         }
-//     })
-
-fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&order_by=name_a_z&client_id=9RQI1WBCZA")
-    .then( res => res.json() )
-    .then( data =>
-        {
-            // Define the event emitter for our new custom event
-            const gameDataRetrieved = new CustomEvent(
-                'games_retrieved',
-                {
-                    // set our API's data into custom properties of the event's detail object
-                    detail:
-                    {
-                        // we can pass anything we want as a key-value pair here, neat!
-                        count: data.count,
-                        games: data.games
-                    }
-                }
-            )
-
-            // dispatch our event using the HTML object it is attached to
-            window.dispatchEvent(gameDataRetrieved)
-    }
-)
-.catch( err =>
-    {
-        console.log('ERROR: ', err);
-    }
-);
+// )
+// .catch( err =>
+//     {
+//         console.log('ERROR: ', err);
+//     }
+// );
 
 
 
 // TEMPLATE MANIPULATION
 
 // Clones feature card template 7 times, adds classes and src to card,
-//  then appends to featured section of page
+//  then appends to featured section of page(randomly selected from usr library for now)
 
 
 function getFeatureCards() {
@@ -87,7 +104,7 @@ function getFeatureCards() {
         itemDiv = tempDiv.querySelector("div");
         itemDiv.setAttribute("class", "feature-card")
         imgItem = itemDiv.querySelector("img").cloneNode(true);
-        imgPath = images[i];
+        imgPath = images[Math.floor(Math.random() * 100)];
         imgItem.setAttribute("src", imgPath);
         imgItem.setAttribute("class", "gallery-img");
         imgItem.setAttribute("alt", "a picture of a game")
@@ -103,7 +120,7 @@ function getRecentGames(){
     let itemDiv, imgItem, imgPath, i, temp, tempDiv, gameItem, gameName;
     temp  = document.getElementById("game-card-template");
 
-    for(i = 0; i < 16; i++){
+    for(i = 0; i < 8; i++){
         tempDiv = temp.content.cloneNode(true);
         itemDiv = tempDiv.querySelector("div");
         itemDiv.setAttribute("class", "game-card grid-box");
@@ -228,4 +245,12 @@ function pickTab(tabName){
         tab.classList.remove('active');
     })
     document.getElementById(tabName).classList.add('active');
+}
+
+function getGame(){
+    
+}
+
+function addGame(game){
+
 }
