85c67a4114a986e50079109ef675e9b241188058
Mark <mwhancock2@gmail.com>
2023-04-29 16:42:02 -0230

    switch back to bga api

--- script.js
+++ script.js
@@ -9,36 +9,18 @@ let lastSlide;
 let images = [];
 let names = [];
 let descriptions = [];
-let hotList = [];
-let hotIds = [];
+let gameIDs = [];
 
 
 // Add event listener to window that will populate library once api is fetched
 window.addEventListener('games-retrieved', e => {
     const gameList = e.detail.games;
-    const gamePics = gameList.getElementsByTagName("image");
-    const gameNames = gameList.getElementsByTagName("name");
-    const gameDes = gameList
-    
-    // If image exists, push it to "images" array
-    for(let img in gamePics){
-        if(gamePics[img].childNodes == undefined){
-            continue
-        }
-        images.push(gamePics[img].childNodes[0].nodeValue);
-    }
-
-    // If name exists, push it to "names" array
-    for(let name in gameNames){
-        if(gameNames[name].childNodes == undefined){
-            continue
-        }
-        names.push(gameNames[name].childNodes[0].nodeValue);
-    }   
-
-    for(let des in gameDes)  {
-
-    }  
+    gameList.forEach(game => {
+        images.push(game.images.medium);
+        names.push(game.name);
+        descriptions.push(game.description_preview);
+        gameIDs.push(game.id)
+    })
 
 
     getFeatureCards();
@@ -50,55 +32,86 @@ window.addEventListener('games-retrieved', e => {
     getRecentGames();
 })
 
-window.addEventListener('hot-list', e => {
-    const gameList = (e.detail.games);
-    const gameIds = gameList.getElementsByTagName("item");
-    const gamePics = (gameList.getElementsByTagName("thumbnail"));
-    for(let  id in gameIds){
-        if(gameIds[id] == undefined){
-            continue
-        }
-        hotIds.push(gameIds[id].id)
-    }
+// window.addEventListener('hot-list', e => {
+//     const gameList = (e.detail.games);
+//     const gameIds = gameList.getElementsByTagName("item");
+//     const gamePics = (gameList.getElementsByTagName("thumbnail"));
+//     for(let  id in gameIds){
+//         if(gameIds[id] == undefined){
+//             continue
+//         }
+//         hotIds.push(gameIds[id].id)
+//     }
 
-    for(let img in gamePics){
-        if(gamePics[img].childNodes == undefined){
-            continue;
-        }
-        hotList.push(gamePics[img].attributes[0].nodeValue)
-    }
+//     for(let img in gamePics){
+//         if(gamePics[img].childNodes == undefined){
+//             continue;
+//         }
+//         hotList.push(gamePics[img].attributes[0].nodeValue)
+//     }
 
-})
+// })
 
 // Boardgamegeek API
 
-fetch("https://boardgamegeek.com/xmlapi2/collection?username=mwhancock&own=1")
-    .then(res => {return res.text()})
-    .then(data => {
-        const gameDataRetrieved = new CustomEvent("games-retrieved",{
-            detail:{
-                games: new DOMParser().parseFromString(data, "text/xml")
-            }
-        })
-        window.dispatchEvent(gameDataRetrieved)
-    })
-    .catch(err => {
-        console.log(`ERROR: ${err}`)
-    })
+// fetch("https://boardgamegeek.com/xmlapi2/collection?username=mwhancock&own=1")
+//     .then(res => {return res.text()})
+//     .then(data => {
+//         const gameDataRetrieved = new CustomEvent("games-retrieved",{
+//             detail:{
+//                 games: new DOMParser().parseFromString(data, "text/xml")
+//             }
+//         })
+//         window.dispatchEvent(gameDataRetrieved)
+//     })
+//     .catch(err => {
+//         console.log(`ERROR: ${err}`)
+//     })
 
-fetch("https://boardgamegeek.com/xmlapi2/hot?type=boardgame")    
-    .then(res => {return res.text()})
-    .then(data => {
-        const hotGames = new CustomEvent("hot-list", {
-            detail:{
-                games: new DOMParser().parseFromString(data, "text/xml")
-            }
-        })
-        window.dispatchEvent(hotGames)   
-    })
-    .catch(err => {
-        console.log(`ERROR: ${err}`)
-    })
+// fetch("https://boardgamegeek.com/xmlapi2/hot?type=boardgame")    
+//     .then(res => {return res.text()})
+//     .then(data => {
+//         const hotGames = new CustomEvent("hot-list", {
+//             detail:{
+//                 games: new DOMParser().parseFromString(data, "text/xml")
+//             }
+//         })
+//         window.dispatchEvent(hotGames)   
+//     })
+//     .catch(err => {
+//         console.log(`ERROR: ${err}`)
+//     })
+
+
+
+
+
+    //BGA API
+
+    fetch("https://api.boardgameatlas.com/api/search?list_id=ydVBm1JJUr&order_by=name_a_z&client_id=9RQI1WBCZA")
+    .then( res => res.json() )
+    .then( data =>
+        {
+            // Define the event emitter for our new custom event
+            const gameDataRetrieved = new CustomEvent(
+                'games-retrieved',
+                {
+                    // set our API's data into custom properties of the event's detail object
+                    detail:
+                    {
+                        games: data.games
+                    }
+                }
+            )
+
+            // dispatch our event using the HTML object it is attached to
+            window.dispatchEvent(gameDataRetrieved)
+    }
+)
+.catch( err =>
+    {
+        console.log('ERROR: ', err);
+    })    
 
 
 
@@ -335,12 +348,12 @@ function getFeatureCards() {
     let itemDiv, imgItem, imgPath, i, temp, tempDiv;
     temp = document.getElementById("feature-card-template");
 
-    for (i = 0; i < 15; i++) {
+    for (i = 0; i < 10; i++) {
         tempDiv = temp.content.cloneNode(true);
         itemDiv = tempDiv.querySelector("div");
         itemDiv.setAttribute("class", "feature-card")
         imgItem = itemDiv.querySelector("img").cloneNode(true);
-        imgPath = hotList[i];
+        imgPath = images[i];
         imgItem.setAttribute("src", imgPath);
         imgItem.setAttribute("class", "gallery-img");
         imgItem.setAttribute("alt", "a picture of a game")
@@ -483,10 +496,12 @@ function pickTab(tabName){
     document.getElementById(tabName).classList.add('active');
 }
 
-function getGame(){
-    
-}
 
-function addGame(game){
+const addGame = document.querySelector(".new-game-btn");
+addGame.addEventListener("click", () => showSearch());
 
+
+function showSearch(){
+   let search = document.querySelector(".search_wrapper");
+   search.style.visibility = "visible";
 }
