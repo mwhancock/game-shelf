1da1a290f67debf467ea972144dcf33ad35128ad
Mark <mwhancock2@gmail.com>
2023-09-17 23:57:44 -0230

    remove brocken api and begin to implement bgg api

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Global variables
-
 const library = document.getElementById("library-body");
 const gallery = document.getElementById("gallery");
 const galleryImg  = document.getElementsByClassName("gallery-img");
@@ -57,7 +56,7 @@ window.addEventListener("load", () => {
   }
 });
 
-libraryConstructor();
+// libraryConstructor();
 
 
 
@@ -70,54 +69,53 @@ libraryConstructor();
 
 
 
+const getHotIds = async () => {
+   const hotlist = await fetch("https://boardgamegeek.com/xmlapi2/hot?type=boardgame")
+    .then((res) =>  res.text())
+    .then((data) => {
+      const parser = new DOMParser();
+      const xmlData = parser.parseFromString(data, "text/xml");
+      const gameList = Array.from(xmlData.getElementsByTagName("item"));
+      const idList = gameList.map((game) => {
+        return game.getAttribute("id");
+      });
+      return idList
+    })
+    .catch((err) => console.log("ERROR: ", err));
+    return hotlist;
+
+}
 
 
 
+  const getGameDetails = async (gameID) => { 
+    const game = await fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${gameID}`)
+      .then((res) => res.text())
+      .then((data) => {
+        const parser = new DOMParser();
+        const xmlData = parser.parseFromString(data, "text/xml");
+        const gameData = Array.from(xmlData.getElementsByTagName("item"));
+        const gameObj = gameData.map((game) => {
+          return {
+            id: gameID,
+            name: game.querySelector("name").getAttribute("value"),
+            image: game.querySelector("image").textContent.trim(),
+            description: game.querySelector("description").textContent.trim(),
+          }
+        })
+        return gameObj;
+      })
+      .catch((err) => console.log("ERROR: ", err));
+      return game;
+    };
 
-//TEST CODE
-
-    // geekXMLToJSON(gameList);
-
-// window.addEventListener('hot-list', e => {
-//     const gameList = (e.detail.games);
-//     const gameIds = gameList.getElementsByTagName("item");
-//     for(let  id in gameIds){
-//         if(gameIds[id] == undefined){
-//             continue
-//         }
-//         hotListIds.push(gameIds[id].id)
-//     }
-
-//     for(let id in hotListIds){
-//       fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${id}`)
-//         .then(res => {return res.text()})
-//         .then(data => {
-//           let game = new DOMParser().parseFromString(data, "text/xml")
-//           hotList.push(game)
-//         })
-//     }
-//     console.log(hotList)
-
-// })
-
-// // Boardgamegeek API
-
-// fetch("https://boardgamegeek.com/xmlapi2/hot?type=boardgame")    
-//     .then(res => {return res.text()})
-//     .then(data => {
-//         // console.log('Hot Games with Custom Parser', parseHotGamesXml(data));
-//         // console.log('Hot Games with generic Parser', parseXmlFromGeek(data));
-//         const hotGames = new CustomEvent("hot-list", {
-//             detail:{
-//                 games: new DOMParser().parseFromString(data, "text/xml")
-//             }
-//         })
-//         window.dispatchEvent(hotGames)   
-//     })
-//     .catch(err => {
-//         console.log(`ERROR: ${err}`)
-//     })
-
+    getHotIds().then((hotlist) => {
+      const promises = hotlist.map((id) => getGameDetails(id));
+      Promise.all(promises).then((games) => {
+        recGames = games;
+        featureConstructor();
+      });
+    });
 
 
 
@@ -143,128 +141,56 @@ libraryConstructor();
 // };
 
 
-// //convert xml data from board game geek api to json
-// const parseXmlFromGeek = (xml) => {
-//   const parser = new DOMParser();
-//   const xmlData = parser.parseFromString(xml, "text/xml");
-//   const game = xmlData.getElementsByTagName("item")[0];
-//   const gameObj = {};
-//   gameObj.id = game.getAttribute("id");
-//   gameObj.name = game.getElementsByTagName("name")[0].getAttribute("value");
-//   gameObj.image = game.getElementsByTagName("image")[0].innerHTML;
-//   gameObj.description = game.getElementsByTagName("description")[0].innerHTML;
-//   return gameObj;
+  
+// Add event listener to the next button that calls the next slide when clicked
+//   const nextSlide = document.querySelector(".next-btn");
+//   nextSlide.addEventListener("click", () => {
+//     fwdSlide();
+//   });
+
+// // Add event listener to the previous button that calls the previous slide when clicked
+//   const prevSlide = document.querySelector(".prev-btn");
+//   prevSlide.addEventListener("click", () => {
+//   bwdSlide();
+//   });
+//   let slideCards = document.getElementsByClassName("feature-card");
+//   slideArr = Array.from(slideCards);
+//   slideArr.forEach((slide) => {
+//     slide.style.display = "none";
+//   });
+//   slideArr[localStorage.getItem("index")].style.display = "block";
+//   lastSlide = slideArr.length - 1;
+
+
+
+
+// const searchAtlasByName = (e) => {
+//   e.preventDefault();
+
+//   const search_term = search_input.value ?? undefined;
+
+//   if (!search_term) return console.warn("Please provide a Search Value");
+
+//   try {
+//     getAtlasData(`name=${search_term}&fuzzy_match=true&order_by=name&limit=100`)
+//       .then((res) => {
+//         res.forEach((game) => {
+//           const gameObj = {};
+//           gameObj.id = game.id;
+//           gameObj.image = game.images.medium;
+//           gameObj.name = game.name;
+//           searchArr.push(gameObj);
+//         });
+//       })
+//       .then(() => {
+//         searchResultsConstructor();
+//         window.location.href = "#search_results";
+//       });
+//   } catch (error) {
+//     console.log("Error retrieving User Games by ID", error);
+//   }
 // };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Generic method for retrieving data from BGA API. defualts to top 100 ranked games
-const getAtlasData = (params = "order_by=rank&limit=100") => {
-  return fetch(
-    `https://api.boardgameatlas.com/api/search?${params}&client_id=${clientID}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data.games;
-    })
-    .catch((err) => console.log("ERROR: ", err));
-};
-
-// Fetches top 100 games on BGA and creates array of game objects
-getAtlasData("order_by=trending&limit=15").then((games_list) => {
-  games_list.forEach((game) => {
-    const gameObj = {};
-    gameObj.id = game.id;
-    gameObj.image = game?.images?.large;
-    gameObj.name = game.name;
-    gameObj.description = game.description_preview;
-    recGames.push(gameObj);
-  });
-
-  featureConstructor();
-  
-  // Add event listener to the next button that calls the next slide when clicked
-  const nextSlide = document.querySelector(".next-btn");
-  nextSlide.addEventListener("click", () => {
-    fwdSlide();
-  });
-
-  // Add event listener to the previous button that calls the previous slide when clicked
-  const prevSlide = document.querySelector(".prev-btn");
-  prevSlide.addEventListener("click", () => {
-  bwdSlide();
-  });
-  let slideCards = document.getElementsByClassName("feature-card");
-  slideArr = Array.from(slideCards);
-  slideArr.forEach((slide) => {
-    slide.style.display = "none";
-  });
-  slideArr[localStorage.getItem("index")].style.display = "block";
-  lastSlide = slideArr.length - 1;
-});
-
-
-const searchAtlasByName = (e) => {
-  e.preventDefault();
-
-  const search_term = search_input.value ?? undefined;
-
-  if (!search_term) return console.warn("Please provide a Search Value");
-
-  try {
-    getAtlasData(`name=${search_term}&fuzzy_match=true&order_by=name&limit=100`)
-      .then((res) => {
-        res.forEach((game) => {
-          const gameObj = {};
-          gameObj.id = game.id;
-          gameObj.image = game.images.medium;
-          gameObj.name = game.name;
-          searchArr.push(gameObj);
-        });
-      })
-      .then(() => {
-        searchResultsConstructor();
-        window.location.href = "#search_results";
-      });
-  } catch (error) {
-    console.log("Error retrieving User Games by ID", error);
-  }
-};
-
-
-// Fetch game data from BGA API based on id
-const fetchGame = async (gameID) => {
-  const game = await fetch(`https://api.boardgameatlas.com/api/search?ids=${gameID}&client_id=${clientID}`)
-    .then((res) => res.json())
-    .then((data) => {
-      return data.games[0];
-    })
-    return game;
-  }
 
   //sorts user library by name
   const sortLibrary = () => {
@@ -334,18 +260,19 @@ function featureConstructor() {
     desItem = itemDiv.querySelector("p").cloneNode(true);
     nameItem = itemDiv.querySelector("h2").cloneNode(true);
     addBtn = itemDiv.querySelector("button").cloneNode(true);
-    imgPath = recGames[i].image;
+    imgPath = recGames[i][0].image;
+    console.log(imgPath);
     imgItem.setAttribute("src", imgPath);
     addBtn.classList.add("feature-add-btn");
-    addBtn.setAttribute("id", recGames[i].id);
+    addBtn.setAttribute("id", recGames[i][0].id);
     addBtn.innerText = "+"
-    gameName = recGames[i].name;
-    gameDes = recGames[i].description;
+    gameName = recGames[i][0].name;
+    gameDes = recGames[i][0].description;
     if(gameDes === ""){
       gameDes = "No description currently available"
-     } else{
-      gameDes = `${shortenParagraph(gameDes, textLimit(screenWidth))}.`
-    }
+    }//  } else{
+      // gameDes = `${shortenParagraph(gameDes, textLimit(screenWidth))}.`
+    // }
     desItem.innerText = `${gameDes}`;
     nameItem.classList.add("feature-name");
     nameItem.innerText = `${gameName}`
@@ -359,97 +286,97 @@ function featureConstructor() {
     itemDiv.append(desItem);
     gallery.append(itemDiv);
   }
-  addFeaturedGame();
+  // addFeaturedGame();
 }
 
 
-function libraryConstructor() {
-  let itemDiv, imgItem, imgPath, i, temp, tempDiv, nameItem, gameName, removeBtn;
-  temp = document.getElementById("game-card-template");
+// function libraryConstructor() {
+//   let itemDiv, imgItem, imgPath, i, temp, tempDiv, nameItem, gameName, removeBtn;
+//   temp = document.getElementById("game-card-template");
 
-  for (i = 0; i < getUserLibrary().length; i++) {
-    tempDiv = temp.content.cloneNode(true);
-    itemDiv = tempDiv.querySelector("div");
-    itemDiv.setAttribute("id", getUserLibrary()[i].id);
-    imgItem = itemDiv.querySelector("img").cloneNode(true);
-    removeBtn = itemDiv.querySelector("button").cloneNode(true);
-    removeBtn.classList.add("remove-btn");
-    removeBtn.setAttribute("id", getUserLibrary()[i].id);
-    removeBtn.innerText = "X";
-    itemDiv.classList.add("game-card");
-    imgPath = getUserLibrary()[i]?.images?.medium;
-    imgItem.setAttribute("src", imgPath);
-    imgItem.setAttribute("alt", "a picture of a game");
-    imgItem.classList.add("game-img");
-    nameItem = itemDiv.querySelector("h4").cloneNode(true);
-    gameName = getUserLibrary()[i].name;
-    nameItem.classList.add("library-game-name");
-    nameItem.innerText = `${gameName}`;
-    itemDiv.append(imgItem);
-    itemDiv.append(nameItem);
-    itemDiv.append(removeBtn);
-    library.append(itemDiv);
-  }
-}
+//   for (i = 0; i < getUserLibrary().length; i++) {
+//     tempDiv = temp.content.cloneNode(true);
+//     itemDiv = tempDiv.querySelector("div");
+//     itemDiv.setAttribute("id", getUserLibrary()[i].id);
+//     imgItem = itemDiv.querySelector("img").cloneNode(true);
+//     removeBtn = itemDiv.querySelector("button").cloneNode(true);
+//     removeBtn.classList.add("remove-btn");
+//     removeBtn.setAttribute("id", getUserLibrary()[i].id);
+//     removeBtn.innerText = "X";
+//     itemDiv.classList.add("game-card");
+//     imgPath = getUserLibrary()[i]?.images?.medium;
+//     imgItem.setAttribute("src", imgPath);
+//     imgItem.setAttribute("alt", "a picture of a game");
+//     imgItem.classList.add("game-img");
+//     nameItem = itemDiv.querySelector("h4").cloneNode(true);
+//     gameName = getUserLibrary()[i].name;
+//     nameItem.classList.add("library-game-name");
+//     nameItem.innerText = `${gameName}`;
+//     itemDiv.append(imgItem);
+//     itemDiv.append(nameItem);
+//     itemDiv.append(removeBtn);
+//     library.append(itemDiv);
+//   }
+// }
 
-function searchResultsConstructor() {
-  let itemDiv, imgItem, imgPath, i, temp, tempDiv, nameItem, gameName, addBtn;
-  temp = document.getElementById("game-card-template");
+// function searchResultsConstructor() {
+//   let itemDiv, imgItem, imgPath, i, temp, tempDiv, nameItem, gameName, addBtn;
+//   temp = document.getElementById("game-card-template");
 
-  for (i = 0; i < searchArr.length; i++) {
-    tempDiv = temp.content.cloneNode(true);
-    itemDiv = tempDiv.querySelector("div");
-    imgItem = itemDiv.querySelector("img").cloneNode(true);
-    itemDiv.classList.add("search-card");
-    imgItem = itemDiv.querySelector("img").cloneNode(true);
-    addBtn = itemDiv.querySelector("button").cloneNode(true);
-    addBtn.classList.add("add-btn");
-    addBtn.setAttribute("id", searchArr[i].id);
-    addBtn.innerText = "Add"
-    imgPath = searchArr[i].image;
-    imgItem.setAttribute("src", imgPath);
-    imgItem.setAttribute("alt", "Game image");
-    imgItem.classList.add("preview-img");
-    nameItem = itemDiv.querySelector("h4").cloneNode(true);
-    nameItem.classList.add("game-name");
-    gameName = searchArr[i].name;
-    nameItem.innerText = `${gameName}`;
-    itemDiv.append(imgItem);
-    itemDiv.append(nameItem);
-    itemDiv.append(addBtn)
-    itemDiv.setAttribute("id", searchArr[i].id)
-    searchResults.append(itemDiv);
-    document.getElementById("result_content").style.display = "block";
+//   for (i = 0; i < searchArr.length; i++) {
+//     tempDiv = temp.content.cloneNode(true);
+//     itemDiv = tempDiv.querySelector("div");
+//     imgItem = itemDiv.querySelector("img").cloneNode(true);
+//     itemDiv.classList.add("search-card");
+//     imgItem = itemDiv.querySelector("img").cloneNode(true);
+//     addBtn = itemDiv.querySelector("button").cloneNode(true);
+//     addBtn.classList.add("add-btn");
+//     addBtn.setAttribute("id", searchArr[i].id);
+//     addBtn.innerText = "Add"
+//     imgPath = searchArr[i].image;
+//     imgItem.setAttribute("src", imgPath);
+//     imgItem.setAttribute("alt", "Game image");
+//     imgItem.classList.add("preview-img");
+//     nameItem = itemDiv.querySelector("h4").cloneNode(true);
+//     nameItem.classList.add("game-name");
+//     gameName = searchArr[i].name;
+//     nameItem.innerText = `${gameName}`;
+//     itemDiv.append(imgItem);
+//     itemDiv.append(nameItem);
+//     itemDiv.append(addBtn)
+//     itemDiv.setAttribute("id", searchArr[i].id)
+//     searchResults.append(itemDiv);
+//     document.getElementById("result_content").style.display = "block";
 
-  }
+//   }
 
   
-  for(let i = 0; i < addGameBtn.length; i++){
-    addGameBtn[i].addEventListener("click", (e) => {
-      e.preventDefault();
-      const gameID = e.target.id;
-      addGameToLibrary(gameID);
-    })
-    searchArr = [];
-  }}
+//   for(let i = 0; i < addGameBtn.length; i++){
+//     addGameBtn[i].addEventListener("click", (e) => {
+//       e.preventDefault();
+//       const gameID = e.target.id;
+//       addGameToLibrary(gameID);
+//     })
+//     searchArr = [];
+//   }}
 
-  for(let i = 0; i < removeGameBtn.length; i++){
-    removeGameBtn[i].addEventListener("click", (e) => {
-      e.preventDefault();
-      const gameID = e.target.id;
-      removeGameFromLibrary(gameID);
-    })
-  }
+//   for(let i = 0; i < removeGameBtn.length; i++){
+//     removeGameBtn[i].addEventListener("click", (e) => {
+//       e.preventDefault();
+//       const gameID = e.target.id;
+//       removeGameFromLibrary(gameID);
+//     })
+//   }
 
-  const addFeaturedGame = () => {
-  for(let i = 0; i < featureBtn.length; i++){
-    featureBtn[i].addEventListener("click", (e) => {
-      e.preventDefault();
-      const gameID = e.target.id;
-      addGameToLibrary(gameID);
-    })
-  }
-}
+//   const addFeaturedGame = () => {
+//   for(let i = 0; i < featureBtn.length; i++){
+//     featureBtn[i].addEventListener("click", (e) => {
+//       e.preventDefault();
+//       const gameID = e.target.id;
+//       addGameToLibrary(gameID);
+//     })
+//   }
+// }
 
 
 
@@ -460,42 +387,42 @@ function clearSearch() {
 
 
 //Shortens game description to fit on game card
-let screenWidth = window.innerWidth;
+// let screenWidth = window.innerWidth;
 
-const textLimit = (width) => {
-  if(width >= 992){
-    return 713;
-  } else if(width >= 710){
-    return 694;
-  } else if(width >= 576){
-    return 562;
-  } else if(width >= 400){
-    return 425
-  }
-}
+// const textLimit = (width) => {
+//   if(width >= 992){
+//     return 713;
+//   } else if(width >= 710){
+//     return 694;
+//   } else if(width >= 576){
+//     return 562;
+//   } else if(width >= 400){
+//     return 425
+//   }
+// }
 
-const shortenParagraph = (text, limit) => {
+// const shortenParagraph = (text, limit) => {
 
-  if(text.length <= limit){
-    return text; //If description already fits on card, returns
-  }
+//   if(text.length <= limit){
+//     return text; //If description already fits on card, returns
+//   }
 
-  //Find the nearest period within the character limit
-  let periodIndex = text.lastIndexOf(".", limit);
+//   //Find the nearest period within the character limit
+//   let periodIndex = text.lastIndexOf(".", limit);
 
-  if(periodIndex != -1){
-    //Slices description at the nearest period to character limit
-    text = text.slice(0, periodIndex + 1);
-  }else {
-    //If no period, slices at character limit
-    text = text.slice(0, limit);
-  }
+//   if(periodIndex != -1){
+//     //Slices description at the nearest period to character limit
+//     text = text.slice(0, periodIndex + 1);
+//   }else {
+//     //If no period, slices at character limit
+//     text = text.slice(0, limit);
+//   }
 
-  //Remove trailing spaces
-  text = text.replace(/[.\s]+$/, "");
+//   //Remove trailing spaces
+//   text = text.replace(/[.\s]+$/, "");
 
-  return text;
-}
+//   return text;
+// }
 
 
 // SLIDESHOW AUTOMATION
@@ -553,7 +480,7 @@ const welcomeDialog = () => {
   // Check if the modal has been shown before
   if (!localStorage.getItem('modalShown')) {
     
-  //   // Get a reference to the modal and the close button
+  // Get a reference to the modal and the close button
     const modal = document.getElementById('modal');
     const closeButton = document.getElementById('close-btn');
     // Show the modal
